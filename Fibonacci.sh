bca3d7923a03a4a9e2927b3420b3b8ccadf18422
SivaKumar482 <42131966+SivaKumar482@users.noreply.github.com>
2021-03-24 19:20:30 +0530

    Create Fibonacci.sh
    Sh file to print Fibonacci Sequence

---
+++ Fibonacci.sh
@@ -0,0 +1,23 @@
+echo "Number of terms to be generated ?"
+  read n
+function fib
+{
+  x=0
+  y=1
+  i=2
+  echo "Fibonacci Series up to $n terms :"
+  echo "$x"
+  echo "$y"
+  while [ $i -lt $n ]
+  do
+      i=`expr $i + 1 `
+      #echo Value of I is $i
+      z=`expr $x + $y `
+      #echo Value of Z is "$z"
+      echo $z
+      x=$y
+      y=$z
+  done
+}
+r=`fib $n`
+echo "$r"
